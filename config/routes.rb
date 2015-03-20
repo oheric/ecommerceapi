fda871f89e91990644c9213ccb9710745e409e11
oheric <ohericyoung@gmail.com>
2015-03-19 23:44:36 -0700

    update routes to scope api into different versions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json }, 
   contraints: { subdomain: 'api' }, path: '/' do
+
+    scope module: :v1 do 
     # We are going to list our resources here
   end
 end
